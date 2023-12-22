e9450066ba37479e1a8f2e0f8982f82132cd159c
Marieke Westendorp <ma3ke.cyber@gmail.com>
2023-12-22 18:34:36 +0100

    Add ability to fix width of Element
    Actually, this is a pretty sizable refactor of the whole Element module.

--- src/elements/mod.rs
+++ src/elements/mod.rs
@@ -11,6 +11,9 @@ pub mod wrapped_text;
 type UpdateFn<D> = fn(&mut ElementKind<D>, &D);
 
 pub struct Element<D> {
+    /// If `None`, the width is determined based on the [`Element`] content. If set with
+    /// `Some(width)`, this width is taken to be this `Element`s width.
+    width: Option<usize>,
     /// Function that will update `thing` according to some `data` with type `D`.
     inner_update: Option<UpdateFn<D>>, // TODO: Bikeshedded name.
     thing: ElementKind<D>,
@@ -19,6 +22,7 @@ pub struct Element<D> {
 impl<D> Element<D> {
     pub fn new(update: Option<UpdateFn<D>>, thing: ElementKind<D>) -> Self {
         Self {
+            width: None,
             inner_update: update,
             thing,
         }
@@ -32,6 +36,27 @@ impl<D> Element<D> {
         Self::new(Some(update), thing)
     }
 
+    /// Explicitly set a fixed width for this [`Element`].
+    pub fn with_fixed_width(mut self, width: usize) -> Self {
+        self.width = Some(width);
+        self
+    }
+
+    /// Let the width of this [`Element`] adapt based on its content.
+    pub fn with_auto_width(mut self) -> Self {
+        self.width = None;
+        self
+    }
+
+    /// Set the width of this [`Element`] according to its content at the moment this function is
+    /// called.
+    pub fn with_baked_width(mut self, font: &Font) -> Self {
+        self.width = Some(self.block_width(font));
+        self
+    }
+}
+
+impl<D> Element<D> {
     /// Update the inner [`ElementKind`] and subsequently let these children call their own update
     /// functions as well, in the case of a collection [`ElementKind`], such as
     /// [`ElementKind::Row`] or [`ElementKind::Stack`].
@@ -58,46 +83,24 @@ impl<D> Element<D> {
 }
 
 impl<D> DrawBlock for Element<D> {
-    fn block_width(&self, font: &Font) -> usize {
-        self.thing.block_width(font)
-    }
-
-    fn block_height(&self, _font: &Font) -> usize {
-        self.thing.block_height(_font)
-    }
-
-    fn block(&self, font: &Font) -> Block {
-        self.thing.block(font)
-    }
-}
-
-pub enum ElementKind<D> {
-    Space,
-
-    Text(String),
-    Paragraph(WrappedText, usize, usize),
-    Graph(Graph),
-
-    Row(Vec<Element<D>>),
-    Stack(Vec<Element<D>>),
-}
-
-impl<D> DrawBlock for ElementKind<D> {
     // TODO: See, this with the font is where my design starts breaking down. I think the a
     // reference to the font should be part of the element.
     fn block_width(&self, font: &Font) -> usize {
-        match self {
-            ElementKind::Space => font.max_width(),
-            ElementKind::Text(t) => font.determine_width(t),
+        // If the width is set explicitly on the [`Element`], that has priority.
+        if let Some(width) = self.width {
+            return width;
+        }
+
+        // Otherwise, calculate the width from the current content.
+        match &self.thing {
+            ElementKind::Space => font.determine_width("  ") as usize,
+            ElementKind::Text(t) => font.determine_width(&t),
             ElementKind::Paragraph(_, width, _) => *width,
             ElementKind::Graph(g) => g.len(),
-            ElementKind::Row(row) => row
-                .iter()
-                .map(|element| element.thing.block_width(font))
-                .sum(),
+            ElementKind::Row(row) => row.iter().map(|element| element.block_width(font)).sum(),
             ElementKind::Stack(stack) => stack
                 .iter()
-                .map(|element| element.thing.block_width(font))
+                .map(|element| element.block_width(font))
                 .max()
                 .unwrap_or_default(),
         }
@@ -106,57 +109,73 @@ impl<D> DrawBlock for ElementKind<D> {
     // TODO: Consider not relying on font here. May actually be more correct, since we always have
     // the same Font::GLYPH_HEIGHT?
     fn block_height(&self, _font: &Font) -> usize {
-        match self {
+        match &self.thing {
             ElementKind::Space | ElementKind::Text(_) | ElementKind::Graph(_) => Font::GLYPH_HEIGHT,
             ElementKind::Paragraph(_, _, height) => *height,
             ElementKind::Row(row) => row
                 .iter()
-                .map(|element| element.thing.block_height(_font))
+                .map(|element| element.block_height(_font))
                 .max()
                 .unwrap_or_default(),
             ElementKind::Stack(stack) => stack
                 .iter()
-                .map(|element| element.thing.block_height(_font))
+                .map(|element| element.block_height(_font))
                 .sum(),
         }
     }
 
     fn block(&self, font: &Font) -> Block {
+        // TODO: Let this point to the Element's style, once we have that.
         let background = [0xff; PIXEL_SIZE];
         let foreground = [0x77, 0x33, 0x22, 0xff];
 
-        let mut block = Block::new(self.block_width(font), self.block_height(font), background);
+        let width = self.block_width(font);
+        let mut block = Block::new(width, self.block_height(font), background);
 
-        match self {
+        fn draw_text(
+            block: &mut Block,
+            text: &str,
+            width: usize,
+            font: &Font,
+            foreground: [u8; 4],
+            background: [u8; 4],
+        ) {
+            let glyphs = text.chars().flat_map(|ch| font.glyph(ch));
+            let mut x0 = 0;
+            for glyph in glyphs {
+                let glyph_width = glyph.width as usize;
+                if x0 >= width {
+                    break;
+                }
+                for (y, row) in glyph.enumerate() {
+                    for (xg, cell) in row.enumerate() {
+                        let x = x0 + xg;
+                        if x >= width {
+                            continue;
+                        }
+                        // TODO: This may be more efficient than what I did in Graph. May be
+                        // worth investigating which is better.
+                        block.buf[y * width + x] = if cell { foreground } else { background };
+                    }
+                }
+                x0 += glyph_width;
+            }
+        }
+
+        match &self.thing {
             ElementKind::Space => {}
             ElementKind::Text(text) => {
-                let glyphs = text.chars().flat_map(|ch| font.glyph(ch));
-                let width: usize = glyphs.clone().map(|glyph| glyph.width as usize).sum();
-                let mut x0 = 0;
-                for glyph in glyphs {
-                    let glyph_width = glyph.width as usize;
-                    for (y, row) in glyph.enumerate() {
-                        for (xg, cell) in row.enumerate() {
-                            let x = x0 + xg;
-                            // TODO: This may be more efficient than what I did in Graph. May be
-                            // worth investigating which is better.
-                            block.buf[y * width + x] = if cell { foreground } else { background };
-                        }
-                    }
-                    x0 += glyph_width;
-                }
+                draw_text(&mut block, text, width, font, foreground, background)
             }
             ElementKind::Paragraph(text, _, height) => {
                 let mut y = 0;
-                for line in text
-                    .as_ref()
-                    .lines()
-                    .map(|line| ElementKind::<D>::Text(line.to_string()))
-                {
-                    let line_block = line.block(font);
+                for line in text.as_ref().lines().map(|line| line.to_string()) {
+                    let width = font.determine_width(&line);
+                    let mut line_block = Block::new(width, *height, background);
+                    draw_text(&mut line_block, &line, width, font, foreground, background);
                     let line_block_height = line_block.height;
                     block.paint(line_block, 0, y);
-                    y += line_block_height;
+                    y += line_block_height; // FIXME
                     if y > *height {
                         break;
                     }
@@ -175,7 +194,7 @@ impl<D> DrawBlock for ElementKind<D> {
                 }
             }
             ElementKind::Row(row) => {
-                let row_blocks = row.iter().map(|element| element.thing.block(font));
+                let row_blocks = row.iter().map(|element| element.block(font));
                 let mut x = 0;
                 for row_block in row_blocks {
                     let width = row_block.width;
@@ -184,7 +203,7 @@ impl<D> DrawBlock for ElementKind<D> {
                 }
             }
             ElementKind::Stack(stack) => {
-                let stack_blocks = stack.iter().map(|element| element.thing.block(font));
+                let stack_blocks = stack.iter().map(|element| element.block(font));
                 let mut y = 0;
                 for stack_block in stack_blocks {
                     let height = stack_block.height;
@@ -196,4 +215,15 @@ impl<D> DrawBlock for ElementKind<D> {
 
         block
     }
+}
+
+pub enum ElementKind<D> {
+    Space,
+
+    Text(String),
+    Paragraph(WrappedText, usize, usize),
+    Graph(Graph),
+
+    Row(Vec<Element<D>>),
+    Stack(Vec<Element<D>>),
 }
